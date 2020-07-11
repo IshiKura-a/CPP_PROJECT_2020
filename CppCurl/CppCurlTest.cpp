d695c6fa3c4d43c3e2fd8342bbd7b00660347443
IshiKura-a <1563415484@qq.com>
2020-07-12 00:34:55 +0800

    Final Version

--- CppCurl/CppCurlTest.cpp
+++ CppCurl/CppCurlTest.cpp
@@ -7,13 +7,17 @@
 int main()
 {
 
-	std::string latex = "\\sigma(z)=\\frac{1}{1+e^{-z}}";
-	std::cout<<downloadRenderedFormula(latex,"gif");
-	system("pause");
+	//std::string latex = "\\sigma(z)=\\frac{1}{1+e^{-z}}";
+	//downloadRenderedFormula(latex,"latex_formula.gif","gif");
+	//system("pause");
 
 	auto s = imageBase64UrlEncode("img.png");
 	s = "image=" + s;
-	std::string token = "24.2ab38b35c29d7c52e3ae47ce5c63b328.2592000.1596796408.282335-21194210";
+	std::string token = "7A4F0C7C6D1AA836183C6245AF5A546D\
+CA62994D2AD6DF01083A5170573825F8\
+869CE35BD9C4A5770C63CCD9A59CBE5B\
+D63F06E28A2AA8D4251B7222F3DC52BE\
+CACE6DD82CDB6F51353347977A16371B";
 	std::cout << formulaRecognition(s, token) << std::endl;
 	system("pause");
 }
