1bb51cb0c68d9be17227da6a686f09a1671550d1
LukeLIN-web <60426396+LukeLIN-web@users.noreply.github.com>
2020-07-12 09:53:54 +0800

    Add files via upload

---
+++ Node.h
@@ -0,0 +1,77 @@
+#pragma once
+#include<iostream>
+using namespace std;
+#ifndef _Node
+#define _Node
+class Node
+{
+private:
+	char op;
+	double val;
+	Node *left, *right;
+	int isUnary; //
+
+public:
+	Node()
+	{
+		op = '\0';
+		val = 0;
+		left = NULL;
+		right = NULL;
+		isUnary = 0;
+	}
+	double getval()
+	{
+		return val;
+	}
+	char& getop()
+	{
+		return op;
+	}
+	void setop(char c)
+	{
+		op = c;
+	}
+	void setval(double data)
+	{
+		val = data;
+	}
+	Node* getleft()
+	{
+		return left;
+	}
+	Node* getright()
+	{
+		return right;
+	}
+	void setleft(Node* b)
+	{
+		left = b;
+	}
+	void setright(Node* b)
+	{
+		right = b;
+	}
+	~Node()
+	{
+		delete left;
+		delete right;
+	}
+	bool isleaf()
+	{
+		if (left == NULL && right == NULL)
+			return 1;
+		else
+			return 0;
+	}
+	void setisUnary(int num)
+	{
+		isUnary = num;
+	}
+	int getisUnary()
+	{
+		return isUnary;
+	}
+	
+};
+#endif _Node
