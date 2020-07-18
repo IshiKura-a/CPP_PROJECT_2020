bff691410410508f7c52208473c6997a2bf758f4
uninstalle <3170105072@zju.edu.cn>
2020-07-18 08:00:27 +0800

    using dynamic TokenDecode library

--- code/module/cppcurl/TokenDecode.h
+++ code/module/cppcurl/TokenDecode.h
@@ -1,5 +1,11 @@
 #pragma once
+#ifdef TOKENDECODE_EXPORTS
+#define TOKENDECODE_API __declspec(dllexport)
+#else
+#define TOKENDECODE_API __declspec(dllimport)
+#endif
+
 #include <curl/curl.h>
 #include <string>
 
-void url_link(CURL* curl, std::string url_base, std::string encoded_token);
+TOKENDECODE_API void url_link(CURL* curl, std::string url_base, std::string encoded_token);
