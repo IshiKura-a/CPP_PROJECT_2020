86b24a405058082a028c62c7380d823341d53512
IshiKura-a <1563415484@qq.com>
2020-07-23 14:01:58 +0800

    Update view.h

--- code/view/view.h
+++ code/view/view.h
@@ -75,7 +75,7 @@ public:
 	
 	void setCalculateLatexFormula(WorkFunctionNoArg command)
 	{
-		calculateLatexFormula = command;
+		calculateInterface->setCalculateLatexFormula(command);
 	}
 	
 	/******************** function entry ********************/
